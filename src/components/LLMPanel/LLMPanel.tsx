471c1fc10a38798c8cbe8346d01aa58962a66d32
Aleksandr Novikov <aleksansdr.nov.2002@gmail.com>
2025-11-08 18:43:21 +0300

    fix: sticky request datails

--- src/components/LLMPanel/LLMPanel.tsx
+++ src/components/LLMPanel/LLMPanel.tsx
@@ -6,6 +6,7 @@ export const LLMPanel: React.FC = () => {
 	const llmAnalysis = useAppSelector(
 		(state) => state.app.openedRequestData?.llmAnalysis
 	);
+
 	return (
 		<div className="llm-panel">
 			<Code className="llm-panel__content">{llmAnalysis}</Code>
